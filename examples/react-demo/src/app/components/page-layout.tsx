1bca49853de72d5d64c2f6498b171e6232b61671
Richard Taylor <richard@findsunrise.com>
2025-08-15 15:12:44 -0700

    GitHub link

--- examples/react-demo/src/app/components/page-layout.tsx
+++ examples/react-demo/src/app/components/page-layout.tsx
@@ -3,9 +3,9 @@
 import React, { type FC, type PropsWithChildren } from "react"
 import { Grid, List, ListItem, ListItemButton, ListItemText } from "@mui/material"
 
-const NavLink = ({ href, label }: { href: string; label: string }) => (
+const NavLink = ({ href, label, target }: { href: string; label: string; target?: string }) => (
   <ListItem disablePadding>
-    <ListItemButton href={href}>
+    <ListItemButton href={href} target={target}>
       <ListItemText>{label}</ListItemText>
     </ListItemButton>
   </ListItem>
@@ -15,6 +15,7 @@ export const PageLayout: FC<PropsWithChildren> = ({ children }) => (
   <Grid container spacing={2} sx={{ width: "100vw", height: "100vh" }}>
     <Grid size={{ xs: 4, sm: 3, md: 2 }} sx={{ backgroundColor: "grey.50" }}>
       <List>
+        <NavLink href="https://github.com/use-bridge/sdk" label="GitHub" target="_blank" />
         <NavLink href="/payer-search" label="Payer Search" />
         <NavLink href="/soft-eligibility" label="Soft Eligibility" />
         <NavLink href="/hard-eligibility" label="Hard Eligibility" />
